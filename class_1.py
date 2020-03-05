e91d9eb5a2ef2a371e44cb97ca48fcd342ad27a0
daniel_saban <daniel@saban.it>
2020-03-05 18:43:24 +0200

    testis 2 changes daniel

--- class_1.py
+++ class_1.py
@@ -1,1 +1,31 @@
-print('see you at Teder.fm saturday or kuli alma because is cooler?')
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.sofascore.com/it/squadra/calcio/inter/2697')
+
+r_2 = requests.get
+# print(r)
+# print(r.text)
+#
+# print(r.headers)
+#
+# print(r.encoding)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+print(soup.prettify())
+
+# list(soup.children)
+#
+# print(r.status_code)
+#
+# html = list(soup.children)[2]
+# print(html)
+#
+# print([type(item) for item in list(soup.children)])
+#
+#
+# body = list(html.children)[3]
+#
+#
+# p = list(body.children)[1]
+# p.get_text()
