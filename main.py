8b24ee6434e1ef9d8f4d54c4ec57d38be8076394
daniel_saban <daniel@saban.it>
2020-04-03 00:04:38 +0300

    Defined dictionary instead of old list for player details

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 import db_control
 from tqdm import tqdm
 
-TOP3_LEAGUES_URLS = [r"https://www.sofascore.com/tournament/football/italy/serie-a/23",     # base urls of each league
-                     r"https://www.sofascore.com/tournament/football/spain/laliga/8",    # can be expand to more leagues
+TOP3_LEAGUES_URLS = [r"https://www.sofascore.com/tournament/football/italy/serie-a/23",  # base urls of each league
+                     r"https://www.sofascore.com/tournament/football/spain/laliga/8",  # can be expand to more leagues
                      r"https://www.sofascore.com/tournament/football/england/premier-league/17"]
 
 
@@ -16,17 +16,39 @@ def extract_player_info(player_url):
     :param player_url: url of the player on https://www.sofascore.com site.
     :return: list ordered in the following way: [Name, Team, Nationality, Age, Height, Preferred Foot, Position, Shirt Number, Market Value]
     """
-    player_html = BeautifulSoup(requests.get(player_url).text, 'html.parser')       # beautifulsoup of the player
-    player_panel_html = player_html.find_all("h2", class_="styles__DetailBoxTitle-sc-1ss54tr-5 enIhhc")     # html of the most interesting data
+    player_dict = {}
+    player_html = BeautifulSoup(requests.get(player_url).text, 'html.parser')  # beautifulsoup of the player
+    player_panel_html = player_html.find_all("h2",
+                                             class_="styles__DetailBoxTitle-sc-1ss54tr-5 enIhhc")  # html of the most interesting data
     details = str(player_panel_html).replace("<", ">").split(">")
     raw_nationality = player_html.find_all("span", class_="u-pL8")
-    nationality = str(raw_nationality).replace("<", ">").split(">")[-3]
-    player_name = player_url.split("/")[-2]
-    player_data = [player_name, nationality]      # initiating player's data list
+    player_dict['nationality'] = str(raw_nationality).replace("<", ">").split(">")[-3]
+    player_dict['name'] = player_url.split("/")[-2].replace('-', ' ').title()
+    age_flag = True
     for i in range(len(details)):
-        if (r"h2 class=" in details[i] or r"span style" in details[i]) and details[i + 1] != '':        # adding data to the list
-            player_data.append(details[i + 1])
-    return player_data
+        if (r"h2 class=" in details[i] or r"span style" in details[i]) and details[i + 1] != '':  # adding data to the list
+            if age_flag:
+                player_dict['age'] = int(details[i + 1])
+                age_flag = False
+            elif 'cm' in details[i + 1]:
+                player_dict['height'] = int(details[i + 1].split()[0])
+            elif '€' in details[i + 1]:
+                player_dict['market_val_million_euro'] = details[i + 1].split()[0]
+                if 'M' in player_dict['market_val_million_euro']:
+                    player_dict['market_val_million_euro'] = float(player_dict['market_val_million_euro'][: -1])
+                else:
+                    player_dict['market_val_million_euro'] = float(player_dict['market_val_million_euro'][: -1]) / 1000
+            elif details[i + 1] in ['Right', 'Left', 'Both']:
+                player_dict['prefd_foot'] = details[i + 1]
+            elif details[i + 1] in ['G', 'D', 'M', 'F']:
+                player_dict['position'] = details[i + 1]
+            else:
+                player_dict['shirt_num'] = int(details[i + 1])
+    dang_keys = ['age', 'height', 'prefd_foot', 'position', 'shirt_num', 'market_val_million_euro']
+    for key in dang_keys:
+        if key not in player_dict.keys():
+            player_dict[key] = None
+    return player_dict
 
 
 def extract_players_urls(team_url):
@@ -37,9 +59,11 @@ def extract_players_urls(team_url):
     """
     player_count = 0
     players_list = []
-    team_html = BeautifulSoup(requests.get(team_url).text, 'html.parser')  # using bs4 & requests to retrieve html as text
-    all_players_html = team_html.find_all("a", class_="squad__player squad-player u-tC js-show-player-modal ff-medium")  # looking for the player info inside the page
-    html_list = str(all_players_html).split()       # manipulating the text to extract player links
+    team_html = BeautifulSoup(requests.get(team_url).text,
+                              'html.parser')  # using bs4 & requests to retrieve html as text
+    all_players_html = team_html.find_all("a",
+                                          class_="squad__player squad-player u-tC js-show-player-modal ff-medium")  # looking for the player info inside the page
+    html_list = str(all_players_html).split()  # manipulating the text to extract player links
     for line in html_list:
         if "href" in line:
             player_count += 1
@@ -54,14 +78,15 @@ def extract_teams_urls(league_url):
     :return: all teams urlss unique and alphabetically sorted in a list
     """
     team_list = []
-    driver = webdriver.Chrome()         # working with selenium google driver as the data is not in the bs4 html
-    driver.get(league_url)              # mimicking human behaviour and opening league url
-    team_html = BeautifulSoup(driver.page_source, 'html.parser')    # getting the source with selenium, parsing with bs4
-    all_teams_html = team_html.find_all("a", class_="js-link")     # looking after all teams urls
-    html_list = str(all_teams_html).split()         # splitting the string by spaces
+    driver = webdriver.Chrome()  # working with selenium google driver as the data is not in the bs4 html
+    driver.get(league_url)  # mimicking human behaviour and opening league url
+    team_html = BeautifulSoup(driver.page_source, 'html.parser')  # getting the source with selenium, parsing with bs4
+    all_teams_html = team_html.find_all("a", class_="js-link")  # looking after all teams urls
+    html_list = str(all_teams_html).split()  # splitting the string by spaces
     for line in html_list:
         if "href=\"/team" in line and not line.endswith("img"):  # getting all lines containing links
-            team_list.append("https://www.sofascore.com" + line.replace("\"", "").split("=")[-1].split(">")[0])  # appending it to a list
+            team_list.append("https://www.sofascore.com" + line.replace("\"", "").split("=")[-1].split(">")[
+                0])  # appending it to a list
     sorted_teams = sorted(list(set(team_list)))  # sorting and removing duplicates
     return sorted_teams  # returning the list
 
@@ -73,14 +98,15 @@ def main():
     db_control.create()
     watch = tqdm(total=100, position=0)
 
-    for league_url in TOP3_LEAGUES_URLS:        # iterating league links
-        teams = extract_teams_urls(league_url)              # extracting teams out of leagues tables
+    for league_url in TOP3_LEAGUES_URLS:  # iterating league links
+        teams = extract_teams_urls(league_url)  # extracting teams out of leagues tables
         league_name = league_url.split("/")[-2]
-        print("\ngetting teams from " + league_name)      # printing for user "loading"
+        print("\ngetting teams from " + league_name)  # printing for user "loading"
         db_control.write_league([league_name, len(teams)])
 
         for team_url in teams:  # iterating all teams urls
-            player_count, players_list = extract_players_urls(team_url)   # extracting player url which also print to screen player data
+            player_count, players_list = extract_players_urls(
+                team_url)  # extracting player url which also print to screen player data
             team_name = team_url.split('/')[-2]
             db_control.write_teams([team_id, league_id, team_name, player_count])
             db_control.write_players(team_id, players_list)
